4f83363451c1e31bca98961d7fdad6b8908018f6
Mellissa Santos <mellissa03santos@gmail.com>
2023-11-22 01:39:38 -0300

    instalando e configurando cors

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./routes/router');
+const cors = require('cors');
 
 const api = express();
 
 api.use(express.json());
 api.use(router);
+api.use(cors());
 
 module.exports = api;
