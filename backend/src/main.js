43365a7a452695c8814089bf873c033e1e9a805b
Mellissa Santos <mellissa03santos@gmail.com>
2023-11-22 23:39:19 -0300

    implementando requisições à APIs no front

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 
 const api = express();
 
+api.use(cors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 api.use(express.json());
 api.use(router);
-api.use(cors());
 
 module.exports = api;
--- frontend/script.js
+++ frontend/script.js
@@ -7,6 +7,7 @@ const email = document.querySelector('#email');
 const street = document.querySelector('#street');
 const number = document.querySelector('#number');
 const complement = document.querySelector('#complement');
+const neighborhood = document.querySelector('#neighborhood');
 const city = document.querySelector('#city');
 const zipCode = document.querySelector('#zip_code');
 const state = document.querySelector('#state');
@@ -14,8 +15,58 @@ const report = document.querySelector('#report');
 const reportType = document.querySelector('#report_type');
 const resident = document.querySelector('#resident');
 const image = document.querySelector('#image');
+// another elements
+const searchButton = document.querySelector('#search_cep');
+const sendButton = document.querySelector('#send_report');
+const anonymousCheckbox = document.querySelector('#anonymous');
+const userSection = document.querySelector('#user_identification');
 
-const button = document.querySelector('#send_report');
+anonymousCheckbox.addEventListener('change', () => {
+    if (anonymousCheckbox.checked) {
+        console.log(userSection);
+        userSection.style.display = 'initial';
+    } else {
+        userSection.style.display = 'none';
+    }
+});
+
+function resetLocationFields() {
+    [street, neighborhood, city, state].forEach((field) => {
+        field.value = '';
+        field.disabled = false;
+    })
+}
+
+function fillLocationFields(data) {
+    resetLocationFields();
+    if (data.logradouro) {
+        street.value = data.logradouro;
+        street.disabled = true;
+    };
+    if (data.bairro) {
+        neighborhood.value = data.bairro;
+        neighborhood.disabled = true;
+    };
+    if (data.localidade) {
+        city.value = data.localidade;
+        city.disabled = true;
+    };
+    if (data.uf) {
+        state.value = data.uf;
+        state.disabled = true;
+    };
+}
+
+async function getDataFromCep() {
+    const cep = zipCode.value;
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    if (response.status !== 200 || data.erro) {
+        return alert('CEP inválido.')
+    }
+    fillLocationFields(data);
+    return console.log(data);
+}
 
 async function postFormData() {
     const payload = {
@@ -29,7 +80,7 @@ async function postFormData() {
             complement: complement.value,
             report: report.value,
             report_type: reportType.value,
-            resident: resident.value,
+            resident: resident.checked ? 1 : 0,
             image: image.value
         },
         user: {
@@ -41,10 +92,12 @@ async function postFormData() {
     }
     const apiConfiguration = {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
     }
-    await fetch('http://localhost:3001/report/', apiConfiguration)
+    await fetch('http://localhost:3001/report', apiConfiguration);
+    console.log(payload);
 }
 
-button.addEventListener('click', postFormData);
+searchButton.addEventListener('click', getDataFromCep);
+sendButton.addEventListener('click', postFormData);
